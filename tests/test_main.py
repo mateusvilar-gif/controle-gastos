e50acb6f613cfe452ef332ec3ebf376b6cf12ac5
mateusvilar-gif <mateus.vilar@sempreceub.com>
2026-04-10 10:13:25 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,12 @@
+def test_soma():
+    assert 2 + 2 == 4
 
+
+def test_valor_positivo():
+    valor = 10
+    assert valor > 0
+
+
+def test_lista_vazia():
+    gastos = []
+    assert len(gastos) == 0
